c388f6cce4aa794114a19cf6d48b732f4177e6cf
hike10000miles <chenshen4009@gmail.com>
2015-11-23 11:42:52 -0500

    Add user following

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -25,4 +25,22 @@ class UserMailerTest < ActionMailer::TestCase
     assert_match CGI::escape(user.email), mail.body.encoded
   end
 
+  test "feed should have the right posts" do 
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from followed user
+    lana.microposts.each do |post_following|  
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
+
 end
